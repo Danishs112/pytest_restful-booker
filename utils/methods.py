91c8086579964f0f198c3b6bdc774edcb2db5d15
Danish Soma <danishsoma112@gmail.com>
2024-10-23 01:01:31 +0530

    added .idea file on the gitignore

--- utils/methods.py
+++ utils/methods.py
@@ -9,30 +9,22 @@ class Methods:
     """ GET Method"""
     @staticmethod
     def get(url):
-   #     Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Methods.headers, cookies=Methods.cookie)
-        #print("getall",result.text)
         Assertions.check_status_code(result,200)
-       #Logger.add_response(result)
         return result
 
     """ POST Method """
     @staticmethod
     def post(url, body):
-    #    Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Methods.headers, cookies=Methods.cookie)
         Assertions.check_status_code(result, 200)
-     #   Logger.add_response(result)
         return result
 
     """ PATCH Methods"""
     @staticmethod
     def patch(url, body, headers):
-      #  Logger.add_request(url, method="PATCH")
         result = requests.patch(url, json=body, headers=headers, cookies=Methods.cookie)
         Assertions.check_status_code(result, 200)
-   #    print(
-      #    Logger.add_response(result)
         return result
 
     """ DELETE Methods"""
